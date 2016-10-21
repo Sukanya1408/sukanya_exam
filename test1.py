69be2ee7bb3ad334e84cedc23970d05f8f8093b9
Sukanya1408 <sukanyasengupta14@gmail.com>
2016-10-21 09:01:24 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+x=int(input("please enter the first number : "))
+y=int(input("please enter the second number : "))
+if (x>y):
+    print("the first value ",x,"is greater")
+elif(y>x):
+    print("the second value ",y,"is greater")
+else:
+    print("the numbers entered are equal")
+      
